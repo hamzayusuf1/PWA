44fab34c54515229b585008167cbf072c2164807
Hamza Yusuf <hamzayusuf26@outlook.com>
2022-05-17 11:27:47 +0100

    service worker updates

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,12 +28,15 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new StaleWhileRevalidate({
+  ({ request }) => request.destination === "image",
+  new CacheFirst({
     cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
   })
